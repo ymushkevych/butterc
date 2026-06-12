e6edab3bcb8a453a59a3c434d25230d02d3edcbb
Yarema Mushkevych <ymushkevych@gmail.com>
2026-06-12 14:57:49 -0700

    Update cleanup.sh

--- build/cleanup.sh
+++ build/cleanup.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "Removing raw assembly"
-[ -f "$./*.asm" ] && rm ./*.asm
-[ -f "$./*.wasm" ] && rm ./*.wasm #in case someone modifies the source code to produce .wasm files
-[ -f "$./*.nasm" ] && rm ./*.nasm #in case someone modifies the source code to produce .nasm files
-[ -f "$./*.fasm" ] && rm ./*.fasm #in case someone modifies the source code to produce .fasm files
+ls *.asm &> /dev/null && rm ./*.asm
+ls *.wasm &> /dev/null && rm ./*.wasm #in case someone modifies the source code to produce .wasm files
+ls *.nasm &> /dev/null && rm ./*.nasm #in case someone modifies the source code to produce .nasm files
+ls *.fasm &> /dev/null && rm ./*.fasm #in case someone modifies the source code to produce .fasm files
 
 echo "Removing object files"
-[ -f "$./*.o" ] && rm ./*.o
-[ -f "$./*.out" ] && rm ./*.out #in case someone modifies the source code to produce .out files
-[ -f "$./*.a" ] && rm ./*.a #in case someone modifies the source code to produce .a files
+ls *.o &> /dev/null && rm ./*.o
+ls *.out &> /dev/null && rm ./*.out #in case someone modifies the source code to produce .out files
+ls *.a &> /dev/null && rm ./*.a #in case someone modifies the source code to produce .a files
